16c2a764b5f1c3ae1a77a86483b4c3b182575dc7
Siraj <sy495p@att.com>
2019-10-20 20:43:46 -0500

    Initial Commit for Smart Badge App

---
+++ Emedallion/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-android-fingerprint-auth.FingerprintAuth",
+      "file": "plugins/cordova-plugin-android-fingerprint-auth/www/FingerprintAuth.js",
+      "pluginId": "cordova-plugin-android-fingerprint-auth",
+      "clobbers": [
+        "FingerprintAuth"
+      ]
+    },
+    {
+      "id": "cordova-plugin-tabrisjs-qrgen.TabrisJSQRCodeGenerator",
+      "file": "plugins/cordova-plugin-tabrisjs-qrgen/www/tabrisjsqrcode.js",
+      "pluginId": "cordova-plugin-tabrisjs-qrgen",
+      "clobbers": [
+        "window.tabrisJsPlugins.qrCode"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-android-fingerprint-auth": "1.5.0",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-tabrisjs-qrgen": "1.0.8"
+  };
+});
