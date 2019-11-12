6b419e582c25b5b7f14fa4322c035953bd6f225a
siraj.yasin <sirajudeen.yasin@gmail.com>
2019-11-12 22:06:48 +0000

    Testing from new VM

--- Emedallion/platforms/android/app/src/main/assets/www/js/scan_giftcode.js
+++ Emedallion/platforms/android/app/src/main/assets/www/js/scan_giftcode.js
@@ -11,7 +11,38 @@ function startScan() {
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
 			var amnt = $('#gift_amnt').text();
-                        $('#gift_amnt').html(parseInt(amnt) + parseInt(result.text));
+			var total_amnt = parseInt(amnt) + parseInt(result.text);
+                        $('#gift_amnt').html(total_amnt);
+
+                        ebucks_json = {};
+                        $.ajax({
+                           url: "https://api.myjson.com/bins/kxd08",
+                           type: "GET",
+                           contentType: "application/json; charset=utf-8",
+                           dataType: "json",
+                           success: function (data, textStatus, jqXHR) {
+               	             ebucks_json = data;
+                           }
+                         });
+
+                     var signum = window.localStorage.getItem('Signum');
+                     
+                      if ( signum ==  null ) {
+	                   signum = 'efgjkmp';
+	              }
+		      ebucks_json[signum]['balance'] = total_amnt;
+                      $.ajax({
+                            url: "https://api.myjson.com/bins/kxd08",
+                            type: "PUT",
+                            contentType: "application/json; charset=utf-8",
+ 	         	    data: JSON.stringify(ebucks_json),
+                            dataType: "json",
+                            success: function (data, textStatus, jqXHR) {
+ 	                       $('#redeem_status').html('Balance Updated!!!');		
+                            }
+                          });
+
+
 		}, 
 		function (error) {
 			alert("Scanning failed: " + error);
